fde7f8fce771be7ca8560086dcd5f6e7921c94b2
nwakalordivine <johndoe@example.com>
2025-07-23 13:35:36 +0100

    recommit

--- api/index.py
+++ api/index.py
@@ -1,7 +1,6 @@
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'app'))
 from app.main import handler as app
-app = app
 # Vercel will look for 'app' in this file
